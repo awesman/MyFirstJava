72b170df3525eb56083fcadf1b655e561d805365
awesman <Draft.92@gmail.com>
2016-11-12 19:38:55 +0300

    new

---
+++ homeWork/src/lesson1/Task1HiJava.java
@@ -0,0 +1,12 @@
+package lesson1;
+
+public class Task1HiJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Привет, Java!");
+
+	}
+
+}
---
+++ homeWork/src/lesson1/Task2HiMaks.java
@@ -0,0 +1,21 @@
+package lesson1;
+
+import java.util.Scanner;
+
+public class Task2HiMaks {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		  Scanner in = new Scanner(System.in);
+
+	        System.out.print("Введите имя: ");
+
+	        String name = in.nextLine();
+
+	        System.out.println("Привет, " + name + "!!!");
+	        in.close();
+
+	}
+
+}
