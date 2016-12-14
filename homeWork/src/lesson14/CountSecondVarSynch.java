97fae5d04c1469c7740fe3cbb62a2275f73f4c43
awesman <Draft.92@gmail.com>
2016-12-14 20:44:13 +0300

    last version

--- homeWork/src/lesson14/CountSecondVarSynch.java
+++ homeWork/src/lesson14/CountSecondVarSynch.java
@@ -1,31 +1,50 @@
 package lesson14;
+
 /**
- *@author Pavel
- *@version 2
+ * Усовершенствовать предыдущее приложение, 
+ * обеспечив синхронизацию, чтобы потоки выводили строки по очереди.
+ * @author Pavel
+ * @version 2
  */
 public class CountSecondVarSynch {
 
 	public static void main(String[] args) {
-		
-		NewThread T1=new NewThread();
-		NewThread T2=new NewThread();
+		Object o = new Object();
+
+		NewThread T1 = new NewThread(o);
+		NewThread T2 = new NewThread(o);
+
 		T1.start();
 		T2.start();
 	}
 }
+
 class NewThread extends Thread {
 	Thread th;
-	public NewThread() {
-		th = new Thread(this);
+	public Object o = new Object();
+	public NewThread(Object o) {
+		this.o = o;
+		th = new Thread();
 	}
+
+	@Override
 	public void run() {
-		synchronized(this){
-			//Cделал немного другой вывод
-		for(int i=0; i<10;i++){
-			System.out.println(th.getName()+":"+" "+(i*10+1)+" "+(i*10+2)+" "+(i*10+3)+" "+(i*10+4)+" "+(i*10+5)+" "+(i*10+6)+" "+(i*10+7)+" "+(i*10+8)+" "+(i*10+9)+" "+(i*10+10));
-			 // Не могу понять почему, но работает правильно в 80% гдето,именно 1 раз сбиваясь иногда
-			yield();  
-		  }
+		synchronized (o) {
+			// Cделал немного другой вывод
+			for (int i = 0; i < 10; i++) {
+				System.out.println(th.getName() + ":" + " "
+						+ (i * 10 + 1) + " " + (i * 10 + 2) + " "
+						+ (i * 10 + 3) + " " + (i * 10 + 4) + " "
+						+ (i * 10 + 5) + " " + (i * 10 + 6) + " "
+						+ (i * 10 + 7) + " " + (i * 10 + 8) + " "
+						+ (i * 10 + 9) + " " + (i * 10 + 10));
+				o.notify();
+				try {
+					o.wait();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
 		}
-     }
+	}
 }
