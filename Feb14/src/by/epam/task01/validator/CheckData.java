af9017277e9de04345082b4d044e671ca5e1f24d
awesman <Draft.92@gmail.com>
2017-02-21 15:38:02 +0300

    homeTask

--- Feb14/src/by/epam/task01/validator/CheckData.java
+++ Feb14/src/by/epam/task01/validator/CheckData.java
@@ -7,11 +7,11 @@ import java.util.regex.Pattern;
 
 public class CheckData {
 
-	private static final Pattern NUMBER_PATTERN =
+	private static final Pattern NUMBER =
 			Pattern.compile(
 					"(\\d)+||(\\d+(//.)\\d+)||((-)(\\d)+)|((-)(\\d+(//.)\\d+))");
-	private static final String SPLIT_BY_SPACES = "\\s+";
-	private static final int LINE_SIZE = 6;
+	private static final String SPACES = "\\s+";
+	private static final int AMOUNT = 6;//amount of numbers that we need for 3 points
 
 	public static ArrayList<String> checkData(
 			List<String> inputData) {
@@ -21,15 +21,15 @@ public class CheckData {
 		
 		for (String line : inputData) {
             int count = 0;
-            strings = line.trim().split(SPLIT_BY_SPACES);
-            if (strings.length == LINE_SIZE) {
+            strings = line.trim().split(SPACES);
+            if (strings.length == AMOUNT) {
                 for (String singleNumber : strings) {
-                    matcher = NUMBER_PATTERN.matcher(singleNumber);
+                    matcher = NUMBER.matcher(singleNumber);
                     if (matcher.find()) {
                         count++;
                     }
                 }
-                if (count == LINE_SIZE) {
+                if (count == AMOUNT) {
                 	triangleCoordinates.add(line);
                 }
             }
